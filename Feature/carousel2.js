f9a91c22352158af1fc8fb5c1702b317ea1ceaca
hackertaco <imsa1224@korea.ac.kr>
2022-04-14 17:52:00 +0900

    finally made carousel

--- Feature/carousel2.js
+++ Feature/carousel2.js
@@ -1,53 +1,79 @@
-let startX = 0;
-let stopX = 0;
-const container = document.querySelector('.card_mob_main_box');
-const slider = document.querySelector('.slider');
-const first = document.getElementsByClassName('card_mob_box')[0]
-const second = document.getElementsByClassName('card_mob_box')[1]
-const third = document.getElementsByClassName('card_mob_box')[2]
-const width = first.getBoundingClientRect().width
-let idx = 0
-let target = ''
-function move(){
-    const moving = startX - stopX;
-    console.log(moving)
-    if(moving > 0){
-        if(moving > width/4){
-            idx !== 2 ? idx+=1 : idx = 2
-            if(moving >width){
-                console.log(moving)
-            }else{
-                slider.style.transform = `translateX(-${(width-moving)*idx}px)`
-            }
+const slider = document.querySelector('.card_mob_container'),
+    slides = Array.from(document.querySelectorAll('.card_mob_box'))
+btns = Array.from(document.querySelectorAll('.direct_btn'))
+// set up our state
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID,
+    currentIndex = 0
 
-            slider.style.transition = 'ease-in-out 0.5s'
+// add our event listeners
+slides.forEach((slide, index) => {
+    slide.addEventListener('touchstart', touchStart(index))
+    slide.addEventListener('touchend', touchEnd)
+    slide.addEventListener('touchmove', touchMove)
+})
 
-        }else{
-            slider.style.transform = `translateX(${moving}px)`
-            slider.style.transition = 'ease-in-out 0.5s'
-        }
-    }else{
-        console.log(moving)
-        if(moving < width/4){
-            console.log(moving)
-            idx-=1
-            idx !== 0 ? idx-=1 : idx = 0
-            slider.style.transform = `translateX(-${(width+moving)*idx}px)`
-            slider.style.transition = 'ease-in-out 0.5s'
-        }
+// prevent menu popup on long press
+window.oncontextmenu = function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+}
 
+function getPositionX(event) {
+    return event.touches[0].clientX
+}
+
+function touchStart(index) {
+    return function (event) {
+        currentIndex = index
+        startPos = getPositionX(event)
+        isDragging = true
+        animationID = requestAnimationFrame(animation)
     }
 }
-function start(e){
-    container.addEventListener('touchend', stop)
-    startX = e.target.getBoundingClientRect().x
-
-}
-function stop(e){
-    stopX = e.target.getBoundingClientRect().x
-
-    move()
-    container.removeEventListener('touchend', stop)
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event)
+        currentTranslate = prevTranslate + currentPosition - startPos
+    }
 }
 
-container.addEventListener('touchstart', start)
+function touchEnd() {
+    cancelAnimationFrame(animationID)
+    isDragging = false
+    const movedBy = currentTranslate - prevTranslate
+
+    // if moved enough negative then snap to next slide if there is one
+    if (movedBy < -100 && currentIndex < slides.length - 1) {
+        btns[currentIndex].classList.remove('now')
+        currentIndex += 1
+    }
+
+    // if moved enough positive then snap to previous slide if there is one
+    if (movedBy > 100 && currentIndex > 0) {
+        btns[currentIndex].classList.remove('now')
+        currentIndex -= 1
+    }
+
+    setPositionByIndex()
+}
+
+function animation() {
+    setSliderPosition()
+    if (isDragging) requestAnimationFrame(animation)
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -(window.innerWidth*0.83)
+    prevTranslate = currentTranslate
+    setSliderPosition()
+    btns[currentIndex].classList.add('now')
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(${currentTranslate}px)`
+}
