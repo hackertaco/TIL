489db9107be9a412e93bc5d80d58775801198359
hackertaco <imsa1224@korea.ac.kr>
2023-04-26 10:00:18 +0900

    C: pointer 복습

--- Language/C/3-pointer.c
+++ Language/C/3-pointer.c
@@ -48,3 +48,55 @@ void main(void)
 
     return 0;
 }
+
+// int 메모리 뷰어
+#include <stdio.h>
+
+int main(void)
+{
+    const int NUM = 0x12345678;
+    const char* NUM_ADDRESS = (char*)&NUM;
+    size_t i;
+
+    for(i=0; i < sizeof(NUM); ++i){
+        printf("%hhx ", *(NUM_ADDRESS+i);
+    }
+
+}
+
+// 두 배열이 겹치는가?
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#define TRUE(1)
+#define FALSE(0)
+#define ARRAY_LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
+
+int is_overlap(int nums[], const size_t length1, int nums2[], const size_t length2);
+
+#endif
+
+#include "memory.h"
+
+int is_overlap(int nums[], const size_t length1, int nums2[], const size_t length2)
+{
+return (nums < nums2 ? nums + length > nums2: nums+length2 > nums)
+}
+
+#include "memory.h"
+
+int main(void)
+{
+    int nums = {1,2,3,4,5};
+    int nums2 = {1,2,4,6};
+    int* nums3 = nums + 2;
+    char* result = NULL;
+
+    const size_t NUMS3_LENGTH 5u;
+
+    result = is_overlap(nums, ARRAY_LENGTH(nums), nums2, ARRAY_LENGTH(nums2)); // no
+        result = is_overlap(nums, ARRAY_LENGTH(nums), nums3, ARRAY_LENGTH(nums3)); // yes
+
+
+
+}
